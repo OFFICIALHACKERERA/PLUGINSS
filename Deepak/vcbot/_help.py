839f734c435d7632baa12d86649e7de98ae1bd03
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-25 10:43:59 +0530

    Add files via upload

--- Deepak/vcbot/_help.py
+++ Deepak/vcbot/_help.py
@@ -10,7 +10,7 @@ from telethon import Button
 from . import vc_asst, asst, in_pattern
 
 
-@vc_asst("^vchelp$")
+@vc_asst("vchelp")
 async def helper(event):
     res = await event.client.inline_query(asst.me.username, "vchelp")
     try:
@@ -19,7 +19,7 @@ async def helper(event):
         await event.eor(e)
 
 
-@in_pattern("^vchelp$")
+@in_pattern("vchelp")
 async def wiqhshd(e):
     builder = e.builder
     res = [
