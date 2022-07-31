db426ba8b1380029acbbd29eb2e6e0cdce91cd56
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-01 00:54:30 +0530

    Update startup.py

--- Deepak/utils/startup.py
+++ Deepak/utils/startup.py
@@ -217,6 +217,27 @@ async def hekp():
     except BaseException:
         pass
 
+spam = os.environ.get("SPAM", None) or "OFF"
+
+
+async def spams():
+    if spam == "ON":
+        import glob
+
+        path = "userbot/plugins/Spam/*.py"
+        files = glob.glob(path)
+        for name in files:
+            with open(name) as f:
+                path1 = Path(f.name)
+                shortname = path1.stem
+                start_spam(shortname.replace(".py", ""))
+    else:
+        print("⚠️Spam Not Loading⚠️")
+
+
+
+
+
 
 async def scammer(username):
     i = 0
