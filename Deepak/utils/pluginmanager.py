1fecc723c51b65ccdaa541f4a4f06b53c1c83e37
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-31 22:14:23 +0530

    Update pluginmanager.py

--- Deepak/utils/pluginmanager.py
+++ Deepak/utils/pluginmanager.py
@@ -13,7 +13,7 @@ from ..helpers.tools import media_type
 from ..helpers.utils import _format, _legendtools, _legendutils, install_pip, reply_id
 from .decorators import admin_cmd, sudo_cmd
 
-LOGS = logging.getLogger("⚡ UserBot.. ⚡")
+LOGS = logging.getLogger("🔥🩸 UƧƎЯᏰø✞ 🩸🔥")
 
 
 def load_module(shortname, plugin_path=None):
@@ -58,7 +58,7 @@ def load_module(shortname, plugin_path=None):
         spec.loader.exec_module(mod)
         # for imports
         sys.modules["Deepak.plugins." + shortname] = mod
-        LOGS.info("💞 UserBot 💞" + shortname)
+        LOGS.info("💞 plugins Installed ❤️ " + shortname)
 
 
 def remove_plugin(shortname):
