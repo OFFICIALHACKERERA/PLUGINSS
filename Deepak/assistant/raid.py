9a032042a9e8835574583d9204224a422abbab3d
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-01 00:14:49 +0530

    Update raid.py

--- Deepak/assistant/raid.py
+++ Deepak/assistant/raid.py
@@ -332,7 +332,6 @@ RAID = [
 @tgbot.on(events.NewMessage(pattern="/raid", func=lambda e: e.sender_id == bot.uid))
 async def spam(e):
     usage = "**CMD** : /raid <value> <text> <reply to anyone>"
-    if Config.SPAM == "ON":
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
         lol = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
@@ -383,7 +382,6 @@ async def raidgoing(event):
 )
 async def replyraid(e):
     global que
-    if Config.SPAM == "ON":
         usage = "/replyraid <reply to anyone>"
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
@@ -407,7 +405,6 @@ async def replyraid(e):
 )
 async def dreplyraid(e):
     global que
-    if Config.SPAM == "ON":
         usage = "/dreplyraid <reply to person> "
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
