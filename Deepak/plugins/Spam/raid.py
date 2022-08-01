0b5fea8068bf9c4b33d3d8c5b7225cfa0a4f42fc
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-01 10:33:52 +0530

    Update raid.py

--- Deepak/plugins/Spam/raid.py
+++ Deepak/plugins/Spam/raid.py
@@ -1,11 +1,7 @@
 import asyncio
-import os
 import random
 
 from telethon import events
-
-from Deepak import bot
-from Deepak.Config import Config
 
 from . import *
 
@@ -329,27 +325,19 @@ RAID = [
 ]
 
 
-ABUSE = os.environ.get("ABUSE", "ON")
-
-
 @tgbot.on(events.NewMessage(pattern="/raid", func=lambda e: e.sender_id == bot.uid))
 async def spam(e):
-    if ABUSE == "ON":
+    usage = "**CMD** : /raid <value> <text> <reply to anyone>"
+    if SPAM == "ON":
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
-        legend = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
-        await e.get_reply_message()
-        if len(legend) == 2:
-            message = str(legend[1])
-            print(message)
-            a = await e.client.get_entity(message)
-            g = a.id
-            c = a.first_name
-            username = f"[{c}](tg://user?id={g})"
-            counter = int(legend[0])
+        lol = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        if len(lol) == 2:
+            message = str(lol[1])
+            counter = int(lol[0])
             for _ in range(counter):
                 reply = random.choice(RAID)
-                caption = f"{username} {reply}"
+                caption = f"{message}"
                 async with e.client.action(e.chat_id, "typing"):
                     await e.client.send_message(e.chat_id, caption)
                     await asyncio.sleep(0.3)
@@ -358,7 +346,7 @@ async def spam(e):
             b = await e.client.get_entity(a.sender_id)
             g = b.id
             c = b.first_name
-            counter = int(legend[0])
+            counter = int(lol[0])
             username = f"[{c}](tg://user?id={g})"
             for _ in range(counter):
                 reply = random.choice(RAID)
@@ -371,7 +359,7 @@ async def spam(e):
 
 
 @tgbot.on(events.NewMessage(incoming=True))
-async def _(event):
+async def raidgoing(event):
     global que
     queue = que.get(event.sender_id)
     if not queue:
@@ -389,25 +377,14 @@ async def _(event):
 @tgbot.on(
     events.NewMessage(pattern="/replyraid", func=lambda x: x.sender_id == bot.uid)
 )
-async def _(e):
+async def replyraid(e):
     global que
-    if ABUSE == "ON":
-        usage = "/replyraid"
+    if SPAM == "ON":
+        usage = "/replyraid <reply to anyone>"
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
-        legend = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
-        await e.get_reply_message()
-        if len(e.text) > 11:
-            message = str(legend[0])
-            a = await e.client.get_entity(message)
-            g = a.id
-            que[g] = []
-            qeue = que.get(g)
-            appendable = [g]
-            qeue.append(appendable)
-            text = f"रिप्लाई रेड स्टार्ट कर दी गई है {Config.ALIVE_NAME}"
-            await e.reply(text, parse_mode=None, link_preview=None)
-        elif e.reply_to_msg_id:
+        lol = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        if e.reply_to_msg_id:
             a = await e.get_reply_message()
             b = await e.client.get_entity(a.sender_id)
             g = b.id
@@ -415,7 +392,7 @@ async def _(e):
             qeue = que.get(g)
             appendable = [g]
             qeue.append(appendable)
-            text = f"**रिप्लाई रेड स्टार्ट कर दी जय है {legend_mention}**"
+            text = f"**Reply Raid Has Been Started By {legend_mention}**"
             await e.reply(text, parse_mode=None, link_preview=None)
         else:
             await e.reply(usage, parse_mode=None, link_preview=None)
@@ -424,25 +401,14 @@ async def _(e):
 @tgbot.on(
     events.NewMessage(pattern="/dreplyraid", func=lambda x: x.sender_id == bot.uid)
 )
-async def _(e):
+async def dreplyraid(e):
     global que
-    if ABUSE == "ON":
-        usage = "/dreplyraid "
+    if SPAM == "ON":
+        usage = "/dreplyraid <reply to person> "
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
-        legend = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
-        await e.get_reply_message()
-        if len(e.text) > 11:
-            message = str(legend[0])
-            a = await e.client.get_entity(message)
-            g = a.id
-            que[g] = []
-            qeue = que.get(g)
-            appendable = [g]
-            qeue.append(appendable)
-            text = "** रिप्लाई रेड रोक दी गई है। **"
-            await e.reply(text, parse_mode=None, link_preview=None)
-        elif e.reply_to_msg_id:
+        lol = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        if e.reply_to_msg_id:
             a = await e.get_reply_message()
             b = await e.client.get_entity(a.sender_id)
             g = b.id
@@ -450,7 +416,7 @@ async def _(e):
             qeue = que.get(g)
             appendable = [g]
             qeue.append(appendable)
-            text = "** रिप्लाई रेड रोक दी गई है। **"
+            text = "** Reply raid has been stopped**"
             await e.reply(text, parse_mode=None, link_preview=None)
         else:
             await e.reply(usage, parse_mode=None, link_preview=None)
