478af26aa736f87403317c8f74910b66775c5937
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-01 09:34:33 +0530

    Update startup.py

--- Deepak/utils/startup.py
+++ Deepak/utils/startup.py
@@ -1,28 +1,37 @@
 import glob
 import os
 import sys
+import urllib.request
 from datetime import timedelta
 from pathlib import Path
 
 from telethon import Button, functions, types, utils
 from telethon.tl.functions.channels import JoinChannelRequest, LeaveChannelRequest
 
-from Deepak import BOTLOG, BOTLOG_CHATID, PM_LOGGER_GROUP_ID
+from Deepak import BOTLOG, BOTLOG_CHATID, PM_LOGGER_GROUP_ID, legendversion
 
 from ..Config import Config
 from ..core.logger import logging
 from ..core.session import legend
-from ..helpers.utils import install_pip
+from ..helpers.utils.utils import runcmd
 from ..sql_helper.global_collection import (
     del_keyword_collectionlist,
     get_item_collectionlist,
 )
 from ..sql_helper.globals import addgvar, gvarstatus
-from .pluginmanager import load_module, start_spam
+from .pluginmanager import load_module
 from .tools import create_supergroup
 
-LOGS = logging.getLogger("Deepak")
+ENV = bool(os.environ.get("ENV", False))
+
+LOGS = logging.getLogger("LegendDeepak")
 cmdhr = Config.HANDLER
+
+
+if ENV:
+    VPS_NOLOAD = ["vps"]
+elif os.path.exists("config.py"):
+    VPS_NOLOAD = ["heroku"]
 
 
 async def setup_bot():
@@ -57,14 +66,14 @@ async def startupmessage():
     """
     Start up message in telegram logger group
     """
+    is_sudo = "True" if Config.SUDO_USERS else "False"
     try:
         if BOTLOG:
             Config.LEGENDUBLOGO = await legend.tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/ddaf037340b883f015b88.jpg",
-                caption="**Your UserBot has been started successfully.**",
-                buttons=[(Button.url("Support", "https://t.me/HEPPYLIFI"),
-                Button.url("Updates", "https://t.me/Broken_Heart_72"),)],
+                "https://telegra.ph/file/294b4dbdb74334fb0a8c1.jpg",
+                caption=f"#START\n\n**__Version__**:- {legendversion}\n\n**__Sudo__** :- {is_sudo}\n\n**Your LegendBot has been started successfully.**",
+                buttons=[(Button.url("Support", "https://t.me/LegendBot_XD"),)],
             )
     except Exception as e:
         LOGS.error(e)
@@ -122,38 +131,63 @@ async def add_bot_to_logger_group(chat_id):
             LOGS.error(str(e))
 
 
-async def load_plugins(folder):
+async def load_plugins(folder, extfolder=None):
     """
     To load plugins from the mentioned folder
     """
-    path = f"Deepak/{folder}/*.py"
+    if extfolder:
+        path = f"{extfolder}/*.py"
+        plugin_path = extfolder
+    else:
+        path = f"Deepak/{folder}/*.py"
+        plugin_path = f"Deepak/{folder}"
     files = glob.glob(path)
     files.sort()
+    success = 0
+    failure = []
     for name in files:
         with open(name) as f:
             path1 = Path(f.name)
             shortname = path1.stem
+            pluginname = shortname.replace(".py", "")
             try:
-                if shortname.replace(".py", "") not in Config.NO_LOAD:
-                    type = True
-                    check = 0
-                    while type:
+                if (pluginname not in Config.NO_LOAD) and (
+                    pluginname not in VPS_NOLOAD
+                ):
+                    flag = True
+                    while flag:
                         try:
                             load_module(
-                                shortname.replace(".py", ""),
-                                plugin_path=f"Deepak/{folder}",
+                                pluginname,
+                                plugin_path=plugin_path,
                             )
+                            if shortname in failure:
+                                failure.remove(shortname)
+                            success += 1
                             break
                         except ModuleNotFoundError as e:
                             install_pip(e.name)
                             check += 1
+                            if shortname not in failure:
+                                failure.append(shortname)
                             if check > 5:
                                 break
                 else:
-                    os.remove(Path(f"Deepak/{folder}/{shortname}.py"))
+                    os.remove(Path(f"{plugin_path}/{shortname}.py"))
             except Exception as e:
-                os.remove(Path(f"Deepak/{folder}/{shortname}.py"))
-                LOGS.info(f"unable to load {shortname} because of error {e}")
+                if shortname not in failure:
+                    failure.append(shortname)
+                os.remove(Path(f"{plugin_path}/{shortname}.py"))
+                LOGS.info(
+                    f"unable to load {shortname} because of error {e}\nBase Folder {plugin_path}"
+                )
+    if extfolder:
+        if not failure:
+            failure.append("None")
+        await legend.tgbot.send_message(
+            BOTLOG_CHATID,
+            f'Your external repo plugins have imported \n**No of imported plugins :** `{success}`\n**Failed plugins to import :** `{", ".join(failure)}`',
+        )
 
 
 async def hekp():
@@ -164,17 +198,35 @@ async def hekp():
     except Exception as e:
         print(str(e))
     try:
-        await legend(JoinChannelRequest("@HEPPYLIFI"))
+        await legend(JoinChannelRequest(""))
     except BaseException:
         pass
     try:
-        await legend(JoinChannelRequest("@Broken_Heart_72"))
+        await legend(JoinChannelRequest(""))
     except BaseException:
         pass
     try:
-        await legend(JoinChannelRequest("@OFFICIALHACKER789"))
+        await legend(LeaveChannelRequest("@Legend_Deepak"))
     except BaseException:
         pass
+    try:
+        await legend(LeaveChannelRequest("@Official_LegendBot"))
+    except BaseException:
+        pass
+
+
+async def scammer(username):
+    i = 0
+    xx = 0
+    async for x in legend.iter_dialogs():
+        if x.is_group or x.is_channel:
+            try:
+                await legend.edit_permissions(x.id, username, view_messages=False)
+                i += 1
+            except:
+                xx += 1
+    print(f"OP {i-xx}")
+
 
 
 spam = os.environ.get("SPAM", None) or "OFF"
@@ -193,6 +245,10 @@ async def spams():
                 start_spam(shortname.replace(".py", ""))
     else:
         print("⚠️Spam Not Loading⚠️")
+
+
+
+
 
 
 async def verifyLoggerGroup():
@@ -226,9 +282,9 @@ async def verifyLoggerGroup():
                 + str(e)
             )
     else:
-        descript = "Don't delete this group or change to group(If you change group all your previous snips, welcome will be lost.)"
+        descript = "A Logger Group For LegendBot.Don't delete this group or change to group(If you change group all your previous snips, welcome will be lost.)"
         _, groupid = await create_supergroup(
-            "UserBot Log Group", legend, Config.BOT_USERNAME, descript
+            "LegendBot Logger", legend, Config.BOT_USERNAME, descript
         )
         addgvar("PRIVATE_GROUP_BOT_API_ID", groupid)
         print(
@@ -261,3 +317,32 @@ async def verifyLoggerGroup():
         args = [executable, "-m", "Deepak"]
         os.execle(executable, *args, os.environ)
         sys.exit(0)
+
+
+async def install_extrarepo(repo, branch, efolder):
+    LEGENDREPO = repo
+    if LEGENDBRANCH := branch:
+        repourl = os.path.join(LEGENDREPO, f"tree/{LEGENDBRANCH}")
+        gcmd = f"git clone -b {LEGENDBRANCH} {LEGENDREPO} {efolder}"
+        errtext = f"There is no branch with name `{LEGENDBRANCH}` in your external repo {LEGENDREPO}. Recheck branch name and correct it in vars(`EXTRA_REPOBRANCH`)"
+    else:
+        repourl = LEGENDREPO
+        gcmd = f"git clone {LEGENDREPO} {efolder}"
+        errtext = f"The link({LEGENDREPO}) you provided for `EXTERNAL_REPO` in vars is invalid. please recheck that link"
+    response = urllib.request.urlopen(repourl)
+    if response.code != 200:
+        LOGS.error(errtext)
+        return await legend.tgbot.send_message(BOTLOG_CHATID, errtext)
+    await runcmd(gcmd)
+    if not os.path.exists(efolder):
+        LOGS.error(
+            "There was a problem in cloning the external repo. please recheck external repo link"
+        )
+        return await legend.tgbot.send_message(
+            BOTLOG_CHATID,
+            "There was a problem in cloning the external repo. please recheck external repo link",
+        )
+    if os.path.exists(os.path.join(efolder, "requirements.txt")):
+        rpath = os.path.join(efolder, "requirements.txt")
+        await runcmd(f"pip3 install --no-cache-dir {rpath}")
+    await load_plugins(folder="Deepak", extfolder=efolder)
