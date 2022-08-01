c5ab38d7a8a4766f879b3fc33724b40175692a59
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-01 10:01:50 +0530

    Update __main__.py

--- Deepak/__main__.py
+++ Deepak/__main__.py
@@ -10,14 +10,14 @@ from .start import killer
 from .utils import (
     add_bot_to_logger_group,
     hekp,
+    install_extrarepo,
     load_plugins,
     setup_bot,
-    spams,
     startupmessage,
     verifyLoggerGroup,
 )
 
-LOGS = logging.getLogger("LegendDeepak")
+LOGS = logging.getLogger("LegendUserBot")
 
 print(Deepak.__copyright__)
 print("Licensed under the terms of the " + Deepak.__license__)
@@ -25,8 +25,15 @@ print("Licensed under the terms of the " + Deepak.__license__)
 cmdhr = Config.HANDLER
 
 
+async def extrarepo():
+    if Config.EXTRA_REPO:
+        await install_extrarepo(
+            Config.EXTRA_REPO, Config.EXTRA_REPOBRANCH, "xtraplugins"
+        )
+
+
 try:
-    LOGS.info("Starting Deepak")
+    LOGS.info("Starting Userbot")
     legend.loop.run_until_complete(setup_bot())
     LOGS.info("TG Bot Startup Completed")
 except Exception as e:
@@ -35,27 +42,30 @@ except Exception as e:
 
 
 async def startup_process():
-    await verifyLoggerGroup()
-    await load_plugins("plugins")
-    await load_plugins("assistant")
-    await killer()
-    await spams()
-    print("----------------")
-    print("Starting Bot Mode!")
-    print("⚜ LegendBot Has Been Deployed Successfully ⚜")
-    print("OWNER - @LegendBoy_XD")
-    print("Group - @LegendBot_XD")
-    print("----------------")
-    await verifyLoggerGroup()
-    await add_bot_to_logger_group(BOTLOG_CHATID)
-    if PM_LOGGER_GROUP_ID != -100:
-        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
-    await startupmessage()
-    return
+    try:
+        await verifyLoggerGroup()
+        await load_plugins("plugins")
+        await load_plugins("assistant")
+        await killer()
+        print("----------------")
+        print("Starting Bot Mode!")
+        print("⚜ LegendBot Has Been Deployed Successfully ⚜")
+        print("OWNER - @LegendBoy_XD")
+        print("Group - @LegendBot_XD")
+        print("----------------")
+        await verifyLoggerGroup()
+        await add_bot_to_logger_group(BOTLOG_CHATID)
+        if PM_LOGGER_GROUP_ID != -100:
+            await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+        await startupmessage()
+        await extrarepo()
+        await hekp()
+    except Exception as e:
+        LOGS.error(f"{str(e)}")
+        sys.exit()
 
 
 legend.loop.run_until_complete(startup_process())
-legend.loop.create_task(hekp())
 
 if len(sys.argv) not in (1, 3, 4):
     legend.disconnect()
