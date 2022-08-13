b36cc675f0740d178ae0d484bf6f54f134bef619
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-14 03:40:19 +0530

    Update inlinebot.py

--- Deepak/core/inlinebot.py
+++ Deepak/core/inlinebot.py
@@ -55,20 +55,20 @@ def ibuild_keyboard(buttons):
 async def on_plug_in_callback_query_handler(event):
     buttons = [
         (
-            Button.inline(f"❮ ᴀᴅᴍɪɴ ❯({len(GRP_INFO['❮ admin ❯'])})", data="admin_menu"),
-            Button.inline(f"❮ ʙᴏᴛ ❯({len(GRP_INFO['❮ bot ❯'])})", data="bot_menu"),
+            Button.inline(f"Admin ({len(GRP_INFO['admin'])})", data="admin_menu"),
+            Button.inline(f"Bot ({len(GRP_INFO['bot'])})", data="bot_menu"),
         ),
         (
-            Button.inline(f"❮ ғᴜɴ ❯({len(GRP_INFO['❮ fun ❯'])})", data="fun_menu"),
-            Button.inline(f"❮ ᴍɪsᴄ ❯({len(GRP_INFO['❮ misc ❯'])})", data="misc_menu"),
+            Button.inline(f"Fun ({len(GRP_INFO['fun'])})", data="fun_menu"),
+            Button.inline(f"Misc ({len(GRP_INFO['misc'])})", data="misc_menu"),
         ),
         (
-            Button.inline(f"❮ ᴛᴏᴏʟs ❯({len(GRP_INFO['❮ tools ❯'])})", data="tools_menu"),
-            Button.inline(f"❮ ᴜᴛɪʟs ❯({len(GRP_INFO['❮ utils ❯'])})", data="utils_menu"),
+            Button.inline(f"Tools ({len(GRP_INFO['tools'])})", data="tools_menu"),
+            Button.inline(f"Utils ({len(GRP_INFO['utils'])})", data="utils_menu"),
         ),
         (
-            Button.inline(f"❮ ᴇxᴛʀᴀ ❯({len(GRP_INFO['❮ extra ❯'])})", data="extra_menu"),
-            Button.inline(f"❮ ᴜsᴇʟᴇss ❯({len(GRP_INFO['❮ useless ❯'])})", data="useless_menu"),
+            Button.inline(f"Extra ({len(GRP_INFO['extra'])})", data="extra_menu"),
+            Button.inline(f"Useless ({len(GRP_INFO['useless'])})", data="useless_menu"),
         ),
         (Button.inline(f"❮ ᴍᴀɪɴ ᴍᴇɴᴜ ❯", data="mainmenu"),),
     ]
