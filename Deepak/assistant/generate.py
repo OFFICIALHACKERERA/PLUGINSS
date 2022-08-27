e1015c072b3beb9f7e34db16f0702fe1f86b1aac
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-28 01:10:51 +0530

    Update generate.py

--- Deepak/assistant/generate.py
+++ Deepak/assistant/generate.py
@@ -1,24 +1,2 @@
-from asyncio.exceptions import TimeoutError
-from bot_pms import bot_pms
-from pyrogram import Client, filters
-from telethon import TelegramClient
-from telethon.sessions import StringSession
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import (
-    ApiIdInvalid,
-    PhoneNumberInvalid,
-    PhoneCodeInvalid,
-    PhoneCodeExpired,
-    SessionPasswordNeeded,
-    PasswordHashInvalid
-)
-from telethon.errors import (
-    ApiIdInvalidError,
-    PhoneNumberInvalidError,
-    PhoneCodeInvalidError,
-    PhoneCodeExpiredError,
-    SessionPasswordNeededError,
-    PasswordHashInvalidError
-)
 
 
