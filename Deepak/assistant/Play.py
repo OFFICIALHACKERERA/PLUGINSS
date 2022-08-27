584d9d433040ad6989b92bc34a4b7a5cca7a5216
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-27 23:55:40 +0530

    Create Play.py

---
+++ Deepak/assistant/Play.py
@@ -0,0 +1,24 @@
+from telethon import events
+
+
+
+
+
+
+
+
+#end
+@DEEPAK.on(events.NewMessage(pattern="^[/?!]end"))
+@is_admin
+async def vc_end(event, perm):
+    chat_id = event.chat_id
+    if chat_id in QUEUE:
+        try:
+            await call_py.leave_group_call(chat_id)
+            clear_queue(chat_id)
+            await event.reply("**Streaming Ended**")
+        except Exception as e:
+            await event.reply(f"**ERROR:** `{e}`")
+    else:
+        await event.reply("**Ntg is Streaming**")
+
