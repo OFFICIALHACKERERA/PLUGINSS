06fe2dddb56ca5f2513db4101b8053252d14d666
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-28 00:32:53 +0530

    Update Play.py

--- Deepak/assistant/Play.py
+++ Deepak/assistant/Play.py
@@ -7,6 +7,141 @@ from . import *
 
 
 
+from pytgcalls import StreamType
+from pytgcalls.types import Update
+from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+from pytgcalls.types.input_stream.quality import (
+    HighQualityAudio,
+    HighQualityVideo,
+    LowQualityVideo,
+    MediumQualityVideo,
+)
+from telethon.tl.functions.channels import LeaveChannelRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from pytgcalls.exceptions import (
+    NoActiveGroupCall,
+    NotInGroupCallError
+)
+from Deepak.status import *
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.functions.messages import ExportChatInviteRequest
+import telethon.utils
+from telethon.tl import functions
+from telethon.tl import types
+from telethon.utils import get_display_name
+from telethon.tl.functions.users import GetFullUserRequest
+from youtubesearchpython import VideosSearch
+
+
+fotoplay = "https://telegra.ph/file/b6402152be44d90836339.jpg"
+ngantri = "https://telegra.ph/file/b6402152be44d90836339.jpg"
+from Deepak import call_py, Deepak, client as Client
+owner = "1669178360"
+from Deepak.helpers.yt_dlp import bash
+from Deepak.helpers.chattitle import CHAT_TITLE
+from Deepak.helpers.queues import (
+    QUEUE,
+    add_to_queue,
+    clear_queue,
+    get_queue,
+    pop_an_item,
+)
+from telethon import Button, events
+from Config import Config
+
+from Deepak.helpers.thumbnail import gen_thumb
+
+
+
+
+
+def vcmention(user):
+    full_name = get_display_name(user)
+    if not isinstance(user, types.User):
+        return full_name
+    return f"[{full_name}](tg://user?id={user.id})"
+
+
+def ytsearch(query: str):
+    try:
+        search = VideosSearch(query, limit=1).result()
+        data = search["result"][0]
+        songname = data["title"]
+        url = data["link"]
+        duration = data["duration"]
+        thumbnail = f"https://i.ytimg.com/vi/{data['id']}/hqdefault.jpg"
+        return [songname, url, duration, thumbnail]
+    except Exception as e:
+        print(e)
+        return 0
+
+
+async def ytdl(format: str, link: str):
+    stdout, stderr = await bash(f'yt-dlp -g -f "{format}" {link}')
+    if stdout:
+        return 1, stdout.split("\n")[0]
+    return 0, stderr
+
+
+async def skip_item(chat_id: int, x: int):
+    if chat_id not in QUEUE:
+        return 0
+    chat_queue = get_queue(chat_id)
+    try:
+        songname = chat_queue[x][0]
+        chat_queue.pop(x)
+        return songname
+    except Exception as e:
+        print(e)
+        return 0
+
+
+async def skip_current_song(chat_id: int):
+    if chat_id not in QUEUE:
+        return 0
+    chat_queue = get_queue(chat_id)
+    if len(chat_queue) == 1:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        return 1
+    songname = chat_queue[1][0]
+    url = chat_queue[1][1]
+    link = chat_queue[1][2]
+    type = chat_queue[1][3]
+    RESOLUSI = chat_queue[1][4]
+    if type == "Audio":
+        await call_py.change_stream(
+            chat_id,
+            AudioPiped(
+                url,
+            ),
+        )
+    elif type == "Video":
+        if RESOLUSI == 720:
+            hm = HighQualityVideo()
+        elif RESOLUSI == 480:
+            hm = MediumQualityVideo()
+        elif RESOLUSI == 360:
+            hm = LowQualityVideo()
+        await call_py.change_stream(
+            chat_id, AudioVideoPiped(url, HighQualityAudio(), hm)
+        )
+    pop_an_item(chat_id)
+    return [songname, link, type]
+
+
+@tgbot.on(events.callbackquery.CallbackQuery(data="cls"))
+async def _(event):
+
+     await event.delete()
+
+btnn =[
+    [Button.url("💁 Sᴜᴘᴘᴏʀᴛ", url=f"t.me/{SUPPORT}"), Button.url("Cʜᴀɴɴᴇʟ 🙋", url=f"t.me/{CHANNEL}")],
+    [Button.inline("Cʟᴏꜱᴇ 🗑️", data="cls")]]*
+
+
+
 
 
 
