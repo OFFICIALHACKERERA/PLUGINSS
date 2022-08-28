a406148a0f6613dd13d85b17a167cf7bf30ac171
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-28 23:28:17 +0530

    Update init.py

--- Deepak/init.py
+++ Deepak/init.py
@@ -8,16 +8,16 @@ import logging
 from pytgcalls import PyTgCalls
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 
-logging.basicsample_config(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
 
 
 from sample_config import sample_config
 BOT_USERNAME = sample_config.BOT_USERNAME
 
-bot = TelegramClient('Deepak', api_id=sample_config.API_ID, api_hash=sample_config.API_HASH)
+bot = TelegramClient('Deepak', api_id=Config.API_ID, api_hash=Config.API_HASH)
 Deepak = bot.start(bot_token=sample_config.BOT_TOKEN)
-client = TelegramClient(StringSession(sample_config.DEEPAK_STRING), sample_config.API_ID, sample_config.API_HASH)
+client = TelegramClient(StringSession(Config.DEEPAK_STRING), sample_config.API_ID, sample_config.API_HASH)
 call_py = PyTgCalls(client)
 client.start()
 call_py.start()
