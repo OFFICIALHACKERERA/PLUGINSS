0e7e0685229ef4231b596eb65cf14b0365eb09af
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-28 23:49:48 +0530

    Update bot.py

--- Deepak/assistant/bot.py
+++ Deepak/assistant/bot.py
@@ -25,15 +25,3 @@ async def _(event):
 
 
 
-@bot.on(events.NewMessage(pattern="^/end"))
-async def vc_end(event, perm):
-    chat_id = event.chat_id
-    if chat_id in QUEUE:
-        try:
-            await call_py.leave_group_call(chat_id)
-            clear_queue(chat_id)
-            await event.reply("**Streaming Ended**")
-        except Exception as e:
-            await event.reply(f"**ERROR:** `{e}`")
-    else:
-        await event.reply("**Ntg is Streaming**")
