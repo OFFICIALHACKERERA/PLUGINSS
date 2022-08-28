6a138fb449e57a3a447e6db41fb56d243dee467e
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-28 22:02:21 +0530

    Add files via upload

--- Deepak/utils.py
+++ Deepak/utils.py
@@ -3,12 +3,12 @@ import logging
 import importlib
 from pathlib import Path
 
-def load_plugins(plugin_name):
-    path = Path(f"Deepak/plugins/{plugin_name}.py")
-    name = "Deepak.plugins.{}".format(plugin_name)
+def load_assistant(plugin_name):
+    path = Path(f"Deepak/assistant/{plugin_name}.py")
+    name = "Deepak.assistant.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["Deepak.plugins." + plugin_name] = load
+    sys.modules["Deepak.assistant." + plugin_name] = load
     print("Bot has Started " + plugin_name)
