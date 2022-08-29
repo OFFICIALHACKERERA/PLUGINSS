99ef755b768d8746b49a3622f23b5db0a8f3691e
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-08-30 00:06:49 +0530

    Update pmpermit.py

--- Deepak/plugins/pmpermit.py
+++ Deepak/plugins/pmpermit.py
@@ -124,7 +124,7 @@ You have {warns}/{totalwarns} warns until you get blocked by the LegendUserBot.
 Choose an option from below to specify the reason of your message and wait for me to check it. __⬇️"""
     else:
         USER_BOT_NO_WARN = f"""__Hi__ {mention}__, I haven't approved you yet to personal message me.
-You have {warns}/{totalwarns} warns until you get blocked by the LegendUserBot.
+You have {warns}/{totalwarns} warns until you get blocked by the Deepak UserBot.
 Don't spam my inbox. say reason and wait until my response.__"""
     addgvar("pmpermit_text", USER_BOT_NO_WARN)
     PM_WARNS[str(chat.id)] += 1
