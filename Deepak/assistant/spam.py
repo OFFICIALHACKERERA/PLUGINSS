c9d52efb6da29bd88a1181a62c2f9603c8d3d396
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-09-01 10:30:39 +0530

    Update spam.py

--- Deepak/assistant/spam.py
+++ Deepak/assistant/spam.py
@@ -422,3 +422,80 @@ async def _(event):
             message="""{}""".format(random.choice(RAID)),
             reply_to=event.message.id,
         )
+
+
+
+
+
+@legend.bot_cmd(events.NewMessage(pattern="/replyraid", func=lambda x: x.sender_id == bot.uid))
+async def _(e):
+    global que
+    if ABUSE == "ON":
+        usage = "/replyraid"
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None)
+        legend = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        await e.get_reply_message()
+        if len(e.text) > 11:
+            message = str(legend[0])
+            a = await e.client.get_entity(message)
+            g = a.id
+            que[g] = []
+            qeue = que.get(g)
+            appendable = [g]
+            qeue.append(appendable)
+            text = f"Activated Reply Raid By {Config.ALIVE_NAME}"
+            await e.reply(text, parse_mode=None, link_preview=None)
+        elif e.reply_to_msg_id:
+            a = await e.get_reply_message()
+            b = await e.client.get_entity(a.sender_id)
+            g = b.id
+            que[g] = []
+            qeue = que.get(g)
+            appendable = [g]
+            qeue.append(appendable)
+            text = f"**Activated Reply Raid By {legend_mention}**"
+            await e.reply(text, parse_mode=None, link_preview=None)
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None)
+
+
+@legend.bot_cmd(
+    events.NewMessage(pattern="/dreplyraid", func=lambda x: x.sender_id == bot.uid)
+)
+async def _(e):
+    global que
+    if ABUSE == "ON":
+        usage = "/dreplyraid "
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None)
+        legend = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        await e.get_reply_message()
+        if len(e.text) > 11:
+            message = str(legend[0])
+            a = await e.client.get_entity(message)
+            g = a.id
+            que[g] = []
+            qeue = que.get(g)
+            appendable = [g]
+            qeue.append(appendable)
+            text = "** Deactivated Reply Raid **"
+            await e.reply(text, parse_mode=None, link_preview=None)
+        elif e.reply_to_msg_id:
+            a = await e.get_reply_message()
+            b = await e.client.get_entity(a.sender_id)
+            g = b.id
+            que[g] = []
+            qeue = que.get(g)
+            appendable = [g]
+            qeue.append(appendable)
+            text = "** Deactivated Reply Raid **"
+            await e.reply(text, parse_mode=None, link_preview=None)
+        else:
+            await e.reply(usage, parse_mode=None, link_preview=None)
+
+
+
+
+
+
