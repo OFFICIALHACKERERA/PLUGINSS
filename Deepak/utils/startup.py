03a55a34dc5a14f789217189365e9dd2de5ad82b
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-09-02 09:40:15 +0530

    Update startup.py

--- Deepak/utils/startup.py
+++ Deepak/utils/startup.py
@@ -203,7 +203,7 @@ async def hekp():
             "DEEPAK_STRING"        
         ] = "String Is A Sensitive Data \nSo Its Protected By LegendBot"
         if OWNER_ID != 0:
-            await legend.tgbot.send_file(
+            await tgbot.send_file(
                 OWNER_ID,
                 DEEPAK_PIC,
                 caption=f"Deployed Userbot Successfully\n\n Userbot \n\nType `.help` or `.ping` to check!\nFor Assistant Type `.on` \n\nJoin [OWNER](t.me/OFFICIALHACKERERA) for Updates & [OFFICIAL CHANNEL](t.me/OFFICIALHACKER789) for any query regarding USERBOT",
