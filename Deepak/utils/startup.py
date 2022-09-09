d77d357c626d67256a76cae33bcce99560fe29f3
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-09-09 10:35:23 +0530

    Update startup.py

--- Deepak/utils/startup.py
+++ Deepak/utils/startup.py
@@ -29,8 +29,9 @@ cmdhr = Config.HANDLER
 
 
 
-LOGGER_ID = os.environ.get("LOGGER_ID", "2035388821")
-       
+LOGGER_ID = os.environ.get("LOGGER_ID", None)
+        if LOGGER_ID:
+            LOGGER_ID = int(LOGGER_ID)
             
 
 LEGEND_PIC = "https://te.legra.ph/file/4f730af88f1d7ec343386.jpg"
