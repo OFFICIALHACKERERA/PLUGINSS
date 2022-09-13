9cc5b039b91030ae6c2e3b11e748aefbb130e5fb
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-09-13 09:12:15 +0530

    Update help.py

--- Deepak/plugins/help.py
+++ Deepak/plugins/help.py
@@ -144,7 +144,7 @@ async def cmdlist():
     },
 )
 async def _(event):
-    "To get guide for LegendBot."
+    "To get guide for UserBot."
     type = event.pattern_match.group(1)
     input_str = event.pattern_match.group(2)
     reply_to_id = await reply_id(event)
